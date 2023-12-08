6b1ba606972eed03c313a157f6d7241c133ba7d1
Carlinhos <cmendonca32@gmail.com>
2023-12-08 21:37:56 +0000

    progress D3P1

--- 3/part1.ex
+++ 3/part1.ex
@@ -4,7 +4,9 @@ symbols_regex = ~r/\*\-\%\/\#\&\$\=\@\+/
 number_regex = ~r/\d{1,3}/
 
 get_pos = fn matrix, {x, y} ->
-  matrix[y][x]
+  matrix
+  |> Enum.at(y)
+  |> Enum.at(x)
 end
 
 kernel1 = fn {x, y} ->
@@ -31,7 +33,7 @@ kernel3 = fn {x, y} ->
   ]
 end
 
-get_kernel = fn len ->
+kernel_factory = fn len ->
   case len do
     1 -> kernel1
     2 -> kernel2
@@ -39,35 +41,60 @@ get_kernel = fn len ->
   end
 end
 
-apply_kernel = fn matrix, kernel, regex ->
-  first_row = Enum.get(kernel, 0)
-  second_row = Enum.get(kernel, 1) |> &(Enum.get(&1, 0) ++ Enum.get(&1, -1))
-  third_row = Enum.get(kernel, 2)
+is_valid_offset = fn {x, y} ->
+  x >= 0 and y >= 0 and x <= 140 and y <= 140
+end
+
+search_offsets = fn matrix, offsets, regex ->
+  first_row =
+    offsets
+    |> Enum.at(0)
+    |> Enum.filter(is_valid_offset)
+
+  second_row =
+    offsets
+    |> Enum.at(1)
+    |> then(&([Enum.at(&1, 0), Enum.at(&1, -1)]))
+    |> Enum.filter(is_valid_offset)
+
+  third_row =
+    offsets
+    |> Enum.at(2)
+    |> Enum.filter(is_valid_offset)
 
   [first_row, second_row, third_row]
+  |> Enum.filter(&(Enum.count(&1) > 0))
   |> Enum.map(fn row ->
     row
     |> Enum.map(&(get_pos.(matrix, &1)))
-    |> Enum.map(&(Regex.match(regex, &1)))
+    |> Enum.map(&(Regex.match?(regex, &1)))
     |> Enum.all?
   end)
   |> Enum.all?
 end
 
-has_symbol_around? = fn matrix, {x, y}, num_length ->
-  kernel = get_kernel.(num_length)
-  positions = kernel.({x, y})
-  apply_kernel.(matrix, positions, symbols_regex)
+has_symbol_around? = fn matrix, {x, y, num_length} ->
+  kernel = kernel_factory.(num_length)
+  offsets = kernel.({x, y})
+  search_offsets.(matrix, offsets, symbols_regex)
+end
+
+string_match_to_pos = fn {idx, length} ->
+  x = rem(idx, 140)
+  y = div(idx, 140)
+  {x, y, length}
 end
 
 matrix = contents
 |> String.split("\n")
-|> Enum.map(String.graphemes)
+|> Enum.map(&String.graphemes/1)
 
 
-Regex.scan(contents, number_regex, return: :index)
-
-
+Regex.scan(number_regex, contents, return: :index)
+|> Enum.map(&hd/1)
+|> Enum.map(string_match_to_pos)
+|> Enum.map(&has_symbol_around?.(matrix, &1))
+|> IO.puts
 
 # Procura os números
 # Em torno de cada número, procurar um símbolo
