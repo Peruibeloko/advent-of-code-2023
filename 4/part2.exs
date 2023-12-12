4468cf043740708e770135481f5a5cf8176efb5a
peruibeloko <cmendonca32@gmail.com>
2023-12-12 13:54:07 -0300

    finish D4P2

--- 4/part2.exs
+++ 4/part2.exs
@@ -1,4 +1,13 @@
-{_, contents} = File.read("test.txt")
+{_, contents} = File.read("input.txt")
+
+pretty_print = fn data, label ->
+  IO.inspect(data,
+    label: label,
+    pretty: true,
+    syntax_colors: IO.ANSI.syntax_colors(),
+    charlists: :as_lists
+  )
+end
 
 parse_line = fn line ->
   line
@@ -48,40 +57,40 @@ binary_search = fn term, array ->
   do_binary_search.(term, array, do_binary_search)
 end
 
-update_games = fn [winners, bets, multiplier], count ->
-  [winners, bets, multiplier + count]
+update_game = fn [winners, bets, current_copies], copies_won ->
+  [winners, bets, current_copies + copies_won]
 end
 
 calculate_points = fn games ->
   do_calculate_points = fn
-    result, [], _ ->
-      result
+    final_result, [], _ ->
+      final_result
 
-    result, [[winners, bets, multiplier] | remaining_games], rec ->
-      IO.inspect(remaining_games)
+    final_result, [[winners, bets, copies_of_current_card] | remaining_games], rec ->
 
-      matches =
+      games_won =
         winners
         |> Enum.filter(&binary_search.(&1, bets))
         |> Enum.count()
 
-      # IO.puts("#{matches}, #{multiplier}")
+      updated_result = final_result + copies_of_current_card
 
-      updated_games =
-        Enum.slice(remaining_games, 0, matches)
-        |> Enum.map(&update_games.(&1, multiplier))
+      affected_games =
+        Enum.slice(remaining_games, 0, games_won)
+        |> Enum.map(&update_game.(&1, copies_of_current_card))
 
-      rec.(result + multiplier, Enum.drop(remaining_games, matches) ++ updated_games, rec)
-  end
+      next_games = affected_games ++ Enum.drop(remaining_games, Enum.count(affected_games))
+
+      # pretty_print.([winners, bets, copies_of_current_card], "---\ncurrent game")
+      # pretty_print.(games_won, "result of game")
+      # pretty_print.(updated_result, "running total")
+      # pretty_print.(next_games, "remaining games")
+
+      rec.(updated_result, next_games, rec)
+    end
 
   do_calculate_points.(0, games, do_calculate_points)
 end
-
-# games = [
-#   [[winning numbers], [your numbers], multiplier],
-#   [[winning numbers], [your numbers], multiplier],
-#   [[winning numbers], [your numbers], multiplier],
-# ]
 
 games =
   contents
@@ -90,7 +99,7 @@ games =
 
 tally = calculate_points.(games)
 
-IO.puts("\n---\nTotal: #{tally}")
+pretty_print.(tally, "---\nTotal card count")
 
 # Pra cada numero ganhador, busca binária nas apostas
 # Pontos = 2 ^ (n - 1)
