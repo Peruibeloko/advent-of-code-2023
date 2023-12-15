bda4b5752d238fb8d5e773a000a373a7d183bc3d
Peruibeloko <cmendonca32@gmail.com>
2023-12-15 19:20:18 -0300

    progress D5P2

--- 5/part2.exs
+++ 5/part2.exs
@@ -9,51 +9,9 @@ defmodule Logging do
       charlists: :as_lists
     )
   end
-
-  def spinner() do
-    IO.puts("|\r")
-    IO.puts("/\r")
-    IO.puts("-\r")
-    IO.puts("\\\r")
-    spinner()
-  end
 end
 
 defmodule Almanac do
-  def get_val(map, key) do
-    map_keys =
-      map
-      |> Map.keys()
-
-    is_special? =
-      map_keys
-      |> Enum.map(&(key in &1))
-      |> Enum.any?()
-
-    # Logging.pretty_print(is_special?, "is #{key} special?")
-
-    if is_special? do
-      src_range = Enum.find(map_keys, &(key in &1))
-      offset = key - src_range.first
-      Map.get(map, src_range) + offset
-    else
-      key
-    end
-  end
-
-  def get_location(seed, maps) do
-    # Logging.pretty_print(seed, "\n\n---\ngetting location for seed")
-
-    maps
-    |> Enum.reduce(seed, fn current_map, prev_result ->
-      # Logging.pretty_print(prev_result, "---\ninput")
-      # Logging.pretty_print(current_map, "map")
-      result = get_val(current_map, prev_result)
-      # Logging.pretty_print(result, "result")
-      result
-    end)
-  end
-
   def parse_raw_map([_ | map_values]) do
     split_spaces_parse_int = fn str ->
       str
@@ -67,12 +25,12 @@ defmodule Almanac do
       Map.put(
         map,
         src_start..(src_start + (range_length - 1)),
-        dest_start
+        dest_start..(dest_start + (range_length - 1))
       )
     end)
   end
 
-  def seed_pair_list_to_range(input) do
+  def seed_pair_to_range(input) do
     pair_to_seed_list = fn [range_start, range_length] ->
       range_start..(range_start + (range_length - 1))
     end
@@ -80,10 +38,6 @@ defmodule Almanac do
     input
     |> Enum.chunk_every(2)
     |> Enum.map(pair_to_seed_list)
-  end
-
-  def min_location_of_seed_range(seed_range, maps) do
-    Enum.reduce(seed_range, &min(&2, Almanac.get_location(&1, maps)))
   end
 
   def parse([raw_seeds | raw_maps]) do
@@ -95,13 +49,34 @@ defmodule Almanac do
       |> hd()
       |> String.split(" ")
       |> Enum.map(&String.to_integer/1)
-      |> seed_pair_list_to_range()
+      |> seed_pair_to_range()
 
     maps =
       raw_maps
       |> Enum.map(&parse_raw_map/1)
 
     {seeds, maps}
+  end
+
+  def split_over_ranges(input_start..input_end, input_stage) do
+    slice_indices = input_stage
+    |> Map.keys()
+    |> Enum.reduce([], fn range_start..range_end, slice_indices ->
+      # How to find the index of
+    end)
+  end
+
+  def process_transformations({seeds, stages}) do
+    stage_affects_seed_range? = fn {input, _}, range_start..range_end ->
+      Enum.member?(input, range_start) or Enum.member?(input, range_end)
+    end
+
+    Enum.reduce(stages, seeds, fn current_stage, previous_result ->
+      Enum.map(previous_result, fn range_to_transform ->
+        affecting_stage_ranges = Enum.filter(current_stage, &stage_affects_seed_range?.(&1, range_to_transform))
+        split_input = split_over_ranges(range_to_transform, affecting_stage_ranges)
+      end)
+    end)
   end
 end
 
@@ -110,9 +85,5 @@ contents
 |> Enum.chunk_by(&(&1 === ""))
 |> Enum.filter(&(&1 !== [""]))
 |> Almanac.parse()
-|> then(fn {seeds, maps} ->
-  task_list = Enum.map(seeds, &Task.async(Almanac, :min_location_of_seed_range, [&1, maps]))
-  Enum.map(task_list, &Task.await(&1, :infinity))
-end)
-|> Enum.min()
+|> Almanac.process_transformations()
 |> Logging.pretty_print("\n\n---\nRecommended location")
